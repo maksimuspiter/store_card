9c6f586bd87698fba0ea40bf7571d29ab9f98264
maksimuspiter <maksimuspiter@gmail.com>
2023-03-07 15:01:03 +0300

    refactor card

--- store/myshop/views.py
+++ store/myshop/views.py
@@ -4,19 +4,22 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from myshop.models import Category, Product
 from card.models import Basket
+from decimal import Decimal
 
 
 def index(request):
     products = Product.objects.all()
     user_products = Basket.objects.filter(user=request.user)
-    # total_cost = not Basket.objects.filter(user=request.user). \
-    #     annotate(price_item=Sum(F('product__price')*F('quantity'))).aggregate(Sum('price_item'))
+
+    total_cost = user_products.\
+        annotate(price_item=Sum(F('product__price')*F('quantity'))).aggregate(Sum('price_item'))['price_item__sum']
     # total_cost = Basket.objects.filter(user=request.user).\
     #     aggregate('product__price')
     # print(total_cost)
 
     total_cont = Basket.objects.filter(user=request.user).aggregate(Sum('quantity'))
-    print(total_cont)
+    # print(total_cont)
     return render(request, 'index.html', {'products': products,
                                           'user_products': user_products,
-                                          'total_cont': total_cont['quantity__sum']})
+                                          'total_cont': total_cont['quantity__sum'],
+                                          'total_cost': '%.2f' % total_cost})
